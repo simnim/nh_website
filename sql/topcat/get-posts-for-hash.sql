fe44a41bcda8c36390518a1126e220086fa995e7
Nick Hahner <nick.hahner@gmail.com>
2022-04-10 21:01:36 -0500

    Added permanent link to top-cat.

--- sql/topcat/get-posts-for-hash.sql
+++ sql/topcat/get-posts-for-hash.sql
@@ -1,25 +1,34 @@
 -- name: get_posts_for_hash
 -- Fetches posts for a given media file media_hash. Optionally with a timestamp.
 select
-      p.url as media
-    , p.title
-    , p.ts_ins as posted_at
-    , p.media_hash as media_hash
-    , group_concat( 'top-' || tp.label || ' @ ' || tp.ts_ins , ' || ') as top_labels
+    p.url as media
+  , p.title
+  , p.ts_ins as posted_at
+  , p.media_hash as media_hash
+  , group_concat(
+          'top-'
+          || tp.label
+          || ' @ '
+          || tp.ts_ins
+          , ' || '
+    ) as top_labels
 from
-    post p
-    left join top_post tp using (post_id)
+  post p
+    left join
+  top_post tp
+    using (post_id)
 where
-      p.media_hash = :media_hash
-      and (
-            :ts_ins is NULL
-            OR p.ts_ins = :ts_ins
-          )
+  p.media_hash = :media_hash
+  and (
+    :ts_ins is NULL
+    OR
+    p.ts_ins = :ts_ins
+  )
 group by
-      p.url
-    , p.title
-    , p.ts_ins
-    , p.media_hash
-
-order by p.ts_ins desc
+    p.url
+  , p.title
+  , p.ts_ins
+  , p.media_hash
+order by
+  p.ts_ins desc
 ;
--- sql/topcat/get-top-posts-for-flask.sql
+++ sql/topcat/get-top-posts-for-flask.sql
@@ -1,13 +1,19 @@
 -- name: get_top_posts_for_flask
 -- Fetches the most recent 10 posts for a particular label
 select
-      p.url as media
-    , p.title
-    , p.ts_ins as noticed_at
-    , p.media_hash
+    p.url as media
+  , p.title
+  , p.ts_ins as noticed_at
+  , p.media_hash
 from
-        top_post tp
-    join post p using (post_id)
-where tp.label = :label
-order by tp.ts_ins desc
-limit 10;
+  top_post tp
+    join
+  post p
+    using (post_id)
+where
+  tp.label = :label
+order by
+  tp.ts_ins desc
+limit
+  10
+;
